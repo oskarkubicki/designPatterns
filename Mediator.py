95dd8105da31a332b515fc182fff084468316b9b
tc7 <tc7@tc7.home>
2020-12-31 16:40:54 -0500

    second task done

--- Mediator.py
+++ Mediator.py
@@ -4,33 +4,25 @@ from abc import ABC
 
 class Mediator(ABC):
 
-
     def notify(self, sender: object, event: str) -> None:
         pass
 
 
-class ConcreteMediator(Mediator):
-    def __init__(self, component1: Component1, component2: Component2) -> None:
+class CalculatorMediator(Mediator):
+    def __init__(self, component1: AdditionComponent, component2: MultiplicationComponent) -> None:
         self._component1 = component1
         self._component1.mediator = self
         self._component2 = component2
         self._component2.mediator = self
 
-    def notify(self, sender: object, event: str) -> None:
-        if event == "adddition":
-            print("Mediator reacts on A and triggers following operations:")
-            self._component2.do_c()
-        elif event == "subtraction":
-            print("Mediator reacts on D and triggers following operations:")
-            self._component1.do_b()
-            self._component2.do_c()
+    def notify(self, sender: object, event: str, number: int,counter:int) -> None:
+        if counter <=1 and event == "addition" :
+            self._component2.multiplication(number)
+        elif counter <=1 and event == "multiplication" and counter:
+            self._component1.addition(number)
 
 
 class BaseComponent:
-    """
-    The Base Component provides the basic functionality of storing a mediator's
-    instance inside component objects.
-    """
 
     def __init__(self, mediator: Mediator = None) -> None:
         self._mediator = mediator
@@ -43,19 +35,28 @@ class BaseComponent:
     def mediator(self, mediator: Mediator) -> None:
         self._mediator = mediator
 
+
 class AdditionComponent(BaseComponent):
 
-    def addition(self,number1:int,number2:int):
-
-        number3=number1+number2
-        self._mediator.notify()
-
+    def addition(self, number1: int):
+        counter= 0
+        number3 = number1 + number1
+        print(number3)
+        self.mediator.notify(self, 'addition', number3,counter)
+        counter += 1
 
 
 class MultiplicationComponent(BaseComponent):
 
-    def addition(self,number1:int,number2:int):
+    def multiplication(self, number1: int):
+        counter= 0
+        number3 = number1 * number1
+        print(number3)
+        self.mediator.notify(self, 'multiplication', number3,counter)
+        counter += 1
 
-        number3 = number1*number2
-        self._mediator.notify()
 
+multiplication = MultiplicationComponent()
+addition = AdditionComponent()
+mediator = CalculatorMediator(addition, multiplication)
+print(addition.addition(5))
